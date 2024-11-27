f70014f434c2eab1674c169eca9e662351b9efaa
harrybandukda <harrisbandukda@gmail.com>
2024-11-27 21:36:56 +0500

    Added api and app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container } from '@mui/material';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import EmployeeList from './components/EmployeeList';
+import EmployeeDetails from './components/EmployeeDetails';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/employees" element={<EmployeeList />} />
+          <Route path="/employee/:id" element={<EmployeeDetails />} />
+        </Routes>
+      </Container>
+    </Router>
   );
-}
+};
 
 export default App;
