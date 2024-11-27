e44fd062874062d13fab3487c451bbfb12a3422c
harrybandukda <harrisbandukda@gmail.com>
2024-11-27 23:04:19 +0500

    Updated CSS

--- frontend/src/components/EditEmployee.js
+++ frontend/src/components/EditEmployee.js
@@ -37,7 +37,7 @@ const EditEmployee = () => {
       alert('Employee updated successfully');
       navigate('/employees');
     } catch (err) {
-      alert('Error updating employee');
+      alert('Error updating employe');
     }
   };
 
